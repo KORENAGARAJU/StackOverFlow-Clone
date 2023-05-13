ce27154fd71279b1e39b54a675d9c0a5066d185c
KORE NAGARAJU <nagaraj13562001@gmail.com>
2023-05-14 02:23:05 +0530

    adding CSS to login page

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import './Auth.css' ;
 import icon from '../../assets/icon.png'
+import AboutAuth from './AboutAuth' 
 
 
 const Auth = ()=>{
@@ -10,21 +11,52 @@ const Auth = ()=>{
     }
     return (
         <section className="auth-section"> 
-           <div className="auth-container">
+           {isSignup && <AboutAuth/>}
+           <div className="auth-container-2">
               { !isSignup && <img src={icon}  alt="slackOverFlow" className="login-logo"/>}
               <form>
+                {
+                    isSignup && (
+                        <label htmlFor="name">
+                            <h4>Display Name</h4>
+                            <input required type="text" id="name" placeholder="Enter Your Name.."/>
+                        </label>
+                    )
+                }
                 <label htmlFor="email">
                     <h4>Email</h4>
                     <input name="email" type="email" id="email" required placeholder="Enter your email"/>
                 </label>
                 <label htmlFor="password">
-                    <div>
+                    <div style={{display:'flex' , justifyContent : 'space-between'}}>
                         <h4>Password</h4>
-                        <h4>Forgot Password?</h4>
+                        {!isSignup && <p style={{color : '#007ac6' , fontSize: '13px'}}>Forgot Password?</p> }
                     </div> 
-                    <input name="password" type="password" required id="password"/>
+                    <input name="password" type="password" placeholder="Enter Password..." required id="password"/>
+                    { isSignup && (<p style={{color : '#666767' , fontSize: '13px'}}>Password must contain at least 8<br/>
+                           Characters,including at least 1 number<br/>
+                           and 1 letter.
+                        </p>)
+                    } 
                 </label>
+                { isSignup && (
+                        <label htmlFor="check">
+                            <input type="checkbox" id="check"/>
+                           <p style={{fontSize: '13px'}}>Opt-in to receive occasional<br/>
+                                product updates,user research invitations,<br/>
+                                company announcements,and digests.
+                           </p>
+                        </label>
+                        )
+                    }
+                 
                 <button type="submit" className="auth-btn">{isSignup ? 'Sign up' : 'Log in' }</button>
+                { isSignup && (
+                    <p style={{color : '#666767' , fontSize: '13px'}}>By clicking "Sign up" you agree to our 
+                     <span style={{color : '#007ac6'}}> terms of<br/>services</span>,
+                     <span style={{color : '#007ac6'}}> privacy policy</span> and 
+                     <span style={{color : '#007ac6'}}> cookies policy</span>.</p>
+                )}
               </form>
               <p>
                 {isSignup ? 'Already have an account?' : "Don't have an account?"}
